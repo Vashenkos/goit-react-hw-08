047041797408c676e41f0e6a047002d616f6142d
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-02 23:14:34 +0200

    іу

--- src/pages/NotFoundPage/RegisterPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/RegisterPage/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import css from "./NotFoundPage.module.css";
 const NotFoundPage = () => {
   return (
     <div className={css.wrapper}>
-      <img className={css.image} src="/public/error.jpg" />
+      <img className={css.image} src="/src/public/error..jpg" />
     </div>
   );
 };
