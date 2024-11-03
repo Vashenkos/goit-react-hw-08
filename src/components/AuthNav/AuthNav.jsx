7da10074e883067323fef0847c330d6354828854
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-04 00:45:29 +0200

    777

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
+import css from "./AuthNav.module.css";
 
 export const AuthNav = () => {
   const buildLinkClass = ({ isActive }) => {
