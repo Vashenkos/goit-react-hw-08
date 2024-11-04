d03e794ef4d5427ff1216c136d80aeb66f3f2b39
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-04 21:06:20 +0200

    q

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";  
 import { PersistGate } from "redux-persist/integration/react";  
 import { BrowserRouter } from "react-router-dom";  
-import { HelmetProvider } from "react-helmet-async";  
+import { HelmetProvider } from 'react-helmet-async';   
 import "./index.css";  
 import "modern-normalize";  
 
