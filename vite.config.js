308ed8413b52ae99bd10084ecf9dd3670f448dd2
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-04 21:14:17 +0200

    2q

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from 'vite';  
 
-export default defineConfig({  
-  build: {  
-    rollupOptions: {  
-      external: ['react-helmet-async'],  
-    },  
-  },  
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
